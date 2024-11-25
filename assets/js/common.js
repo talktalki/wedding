a3cb8a9d2806af5ecc3d9e204402f0bf01eed3fc
eunseoya <gloria.kim@my.wheaton.edu>
2024-11-25 01:18:48 -0500

    fix footer html

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,13 +1,13 @@
 // common.js
 function loadFooter() {
-    data = <footer id="footer">
+    data = `<footer id="footer">
         <ul class="icons">
             <li><a href="https://www.instagram.com/talktalki/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
         </ul>
         <ul class="copyright">
             <li>&copy; talktalki</li>
         </ul>
-    </footer>
+    </footer>`
     document.getElementById('footer-placeholder').innerHTML = data;
 }
 document.addEventListener('DOMContentLoaded', loadFooter);
