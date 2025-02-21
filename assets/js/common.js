20c69ba19e5be9e05ea877fe8e065fbccf0b3e96
eunseoya <gloria.kim@my.wheaton.edu>
2025-02-21 14:42:09 -0500

    add heart effect, add humans.txt

--- assets/js/common.js
+++ assets/js/common.js
@@ -84,3 +84,14 @@ function showCopyFeedback(element, message) {
 }
 
 
+function createHeartShower() {
+    for (let i = 0; i < 20; i++) {
+        const heart = document.createElement('div');
+        heart.className = 'floating-heart';
+        heart.innerHTML = '<svg class="countdown-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="#F6DCD0"/></svg>';
+        heart.style.left = Math.random() * 100 + 'vw';
+        heart.style.animationDelay = Math.random() * 1.5 + 's'; // Reduced from 2s to 0.5s
+        document.body.appendChild(heart);
+        setTimeout(() => heart.remove(), 3000); // Reduced from 3000ms to 1500ms
+    }
+}
